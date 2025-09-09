1fe53ef9c0f795b82994fab27f7dfc37a7a3210c
krishnatkocherla <krishnatkocherla@gmail.com>
2025-09-08 23:26:13 -0400

    transcript summarization setup

--- src/live_transcribe.py
+++ src/live_transcribe.py
@@ -1,13 +1,18 @@
 import sounddevice as sd
 import numpy as np
 import queue
+import torch
+
+from vllm import LLM, SamplingParams
+from copy import deepcopy
 from faster_whisper import WhisperModel
-from transformers import pipeline
+from transformers import pipeline, BitsAndBytesConfig, AutoModelForCausalLM, AutoTokenizer
+from transformers import LogitsProcessor
 
 # ====== CONFIG ======
 SAMPLE_RATE = 16000
 CHUNK_DURATION = 5    # seconds per processing chunk
-MODEL_SIZE = "medium"   # faster-whisper model: tiny, base, small, medium, large-v2
+MODEL_SIZE = "large-v2"   # faster-whisper model: tiny, base, small, medium, large-v2
 # ====================
 
 # Load models
@@ -15,6 +20,23 @@ print("Loading Faster-Whisper model...")
 whisper_model = WhisperModel(MODEL_SIZE, device="cuda", compute_type="float16")
 # print("Loading summarizer...")
 # summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+
+def truncate_length(prompt, tokenizer):
+    temp = tokenizer.encode(prompt)
+    if len(temp) > 32768:
+        temp = temp[:32768]
+        truncated_prompt = tokenizer.decode(temp)
+    else:
+        truncated_prompt = prompt
+    return truncated_prompt
+
+def extract_output(qwen_output):
+    # Extract text after </think>
+    if qwen_output.count("<think>")  == 1 and qwen_output.count("</think>") == 0:
+        return "<FAILED>"
+    after_think = qwen_output.split("</think>", 1)[-1].strip()
+    return after_think
+
 
 # Audio queue
 audio_q = queue.Queue()
